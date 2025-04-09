a24667e9b8f4c194f871b47fa3fb03989bc7de78
Ranmdom <slimegamer12@gmail.com>
2025-04-08 23:02:31 -0300

    Tirando a badge de favorito

--- src/components/exclusives-proprierties.tsx
+++ src/components/exclusives-proprierties.tsx
@@ -208,23 +208,7 @@ export function ExclusiveProperties() {
                             >
                               {formatPrice(Number(property.price))}
                             </p>
-                            <div className="bg-[#fabc3f]/10 rounded-full p-1.5">
-                              <svg
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
-                                  stroke="#fabc3f"
-                                  strokeWidth="2"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                />
-                              </svg>
-                            </div>
+                            
                           </div>
 
                           <div className="flex justify-between text-white">
