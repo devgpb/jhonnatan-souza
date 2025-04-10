b8b6e671768af91c943e75ae0a034011da69f2ea
devgpb <bezerra.pereira.dev@gmail.com>
2025-04-10 12:14:32 -0300

    Finalmente barra de carregamento

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import '@/app/globals.css';
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -12,7 +16,24 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
   const getLayout = Component.getLayout ?? ((page) => page);
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start();
+    const handleStop = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
 
   return getLayout(<Component {...pageProps} />);
 }
